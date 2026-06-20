0bdf05972b8926d286b8b73392380a82fdca1c8d
tglspfdsfgs <tglspfdsfgs@gmail.com>
2026-06-20 14:57:41 +0300

    fix: router link to pizzas catalog view

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/', 'pages::home')->name('home');
-Route::livewire('/pizzas', 'pages::shop.pizza')->name('pizzas');
+Route::livewire('/pizzas', 'pages::shop.pizzas')->name('pizzas');
 Route::livewire('/drinks', 'pages::shop.drinks')->name('drinks');
 Route::livewire('/sides', 'pages::shop.sides')->name('sides');
 Route::livewire('/desserts', 'pages::shop.dessert')->name('desserts');
